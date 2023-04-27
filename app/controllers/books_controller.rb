2c8a246d800a8e4b9f046fe7dddd738bed3e144b
ReturnKivototh <126948762+reternkivototh@users.noreply.github.com>
2023-04-27 13:13:28 +0000

    [Fix] update error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+     flash[:notice] = "Book was successfully created."
      redirect_to book_path(@book.id)
     else
      @books = Book.all
@@ -15,6 +16,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
 
   end
 
@@ -27,9 +29,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
