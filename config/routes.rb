c2ebb91b45bdb1d035c2dbfcdbce9988db4f2c10
ReturnKivototh <126948762+reternkivototh@users.noreply.github.com>
2023-04-28 16:01:28 +0000

    [Fix]

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'books/index'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/edit'
-  get 'top' => 'homes#top'
+  get '/' => 'homes#top'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
